afa6a1fd5f198603ca9fa21f2d838b182ebaaf27
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-06-06 01:08:58 +0530

    trying og

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import "./globals.css"
 const SpaceGrotesk = Space_Grotesk({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "HackerNews Comments Summarizer",
-  description: "Summarize the top comments on HackerNews using AI",
+  title: "What the Hack?!",
+  description:
+    "Get to know the top comments of a Hacker News post in a glance! Powered by Nextjs and Cloudflare Workers AI.",
 }
 
 export default function RootLayout({
@@ -18,12 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-background">
+      <meta property="og:image" content="/opengraph-image.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta name="twitter:image" content="/twitter-image.png" />
+      <meta name="twitter:image:type" content="image/png" />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="630" />
       <body className={SpaceGrotesk.className}>
         {children}
-        <Toaster
-        richColors
-        position="top-center"
-        />
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   )
